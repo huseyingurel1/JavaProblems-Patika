57b74b47cc1ed0d284329218468f7540edbcccc1
huseyingurel1 <huseyingurel27@gmail.com>
2022-05-23 13:50:14 +0300

    taxiMeter

---
+++ src/TaxiMeter/Main.java
@@ -0,0 +1,23 @@
+package TaxiMeter;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int km;
+        double perKm = 2.20 , total , startPrice = 10;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("enter the distance in km : ");
+        km = scan.nextInt();
+
+        total=( km * perKm );
+        total+=startPrice;
+
+        total =(total < 20) ? 20 : total ;
+        System.out.println("the amount you have to pay is  = " + total );
+
+
+    }
+
+}
