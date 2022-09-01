c22221e1c39af50f34a7f9e1d2fad6cc0a7e9063
huseyingurel1 <huseyingurel27@gmail.com>
2022-09-01 14:04:34 +0300

    FindPerfectNumber

---
+++ src/PerfectNumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please Enter a number");
+        int number = scanner.nextInt();
+        int sum = 0;
+
+        for (double i = 1; i < number; i++) {
+
+            if(number%i==0) {
+                sum+=i;
+                System.out.println("sum = " + sum);
+
+            }
+        }
+        System.out.println("sum = " + sum);
+
+        if(sum==number){
+            System.out.println("perfect number");
+        }else{
+            System.out.println("Not a perfect number");
+        }
+
+    }
+}
